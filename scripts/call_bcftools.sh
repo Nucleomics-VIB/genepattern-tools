0c38ba04c5eb1f0d3086af1a746b3280d91e3fde
Stephane Plaisance <stephane.plaisance@vib.be>
2017-08-23 14:20:11 +0200

    edits

--- scripts/call_bcftools.sh
+++ scripts/call_bcftools.sh
@@ -15,6 +15,7 @@ Usage: call_bcftools.sh -i <SAM|BAM.file>
 #   -D <max-depth (default 1000)>
 #   -s <path to samtools_1.5>
 #   -b <path to bcftools_1.5>
+#   -p <ploidy default to 'undef' (diploid)>
 
 # call variants from pileup, filter and convert calls to VCF format
 # samtools mpileup arguments
@@ -27,7 +28,7 @@ Usage: call_bcftools.sh -i <SAM|BAM.file>
 # exclude calls where more than 1000 reads support a variation
 EOF
 
-while getopts "i:x:r:D:s:b:" opt; do
+while getopts "i:x:r:D:s:b:p:t:" opt; do
   case $opt in
     i)
       infile=${OPTARG}
@@ -46,7 +47,13 @@ while getopts "i:x:r:D:s:b:" opt; do
       ;;    
     b)
       bcftoolspath=${OPTARG}
-      ;;  
+      ;;
+    p)
+      ploidy=${OPTARG}
+      ;;
+    t)
+      compthr=${OPTARG}
+      ;; 
     \?)
       echo ${usage}
       exit 1
@@ -95,14 +102,93 @@ indexcp=$(basename ${index})
 cp $index ./${indexcp}
 fi
 
+# handle -p
+if [ ! "${ploidy}" == "\"\"" ]; then
+ploidyval="--ploidy \""${ploidy}"\""
+else
+ploidyval=''
+fi
+
+# compression threads
+thr=${compthr:-4}
+
 # basename
 prefix=$(basename ${infile})
 
+read -d '' cmd << EOF
 ((${samtools} mpileup -uf ${reference} ${infile} | \
-	${bcftools} call -vc -O u > ./${prefix}.raw.bcf) && \
+	${bcftools} call --threads ${thr} ${ploidyval} -vc -O u > ./${prefix}.raw.bcf) && \
 (bcftools view ./${prefix}.raw.bcf | \
 	${vcfutils} varFilter -D${maxdepth} > \
 	./${prefix}_var_bcftools.flt-D${maxdepth}.vcf)) && \
 	rm ./${prefix}.raw.bcf
+EOF
+
+echo "# ${cmd}"
+eval ${cmd}
 
 exit 0
+
+# bcftools call --ploidy ?
+# 
+# PRE-DEFINED PLOIDY FILES
+# 
+#  * Columns are: CHROM,FROM,TO,SEX,PLOIDY
+#  * Coordinates are 1-based inclusive.
+#  * A '*' means any value not otherwise defined.
+# 
+# GRCh37
+#    .. Human Genome reference assembly GRCh37 / hg19
+# 
+# X 1 60000 M 1
+# X 2699521 154931043 M 1
+# Y 1 59373566 M 1
+# Y 1 59373566 F 0
+# MT 1 16569 M 1
+# MT 1 16569 F 1
+# chrX 1 60000 M 1
+# chrX 2699521 154931043 M 1
+# chrY 1 59373566 M 1
+# chrY 1 59373566 F 0
+# chrM 1 16569 M 1
+# chrM 1 16569 F 1
+# *  * *     M 2
+# *  * *     F 2
+# 
+# GRCh38
+#    .. Human Genome reference assembly GRCh38 / hg38
+# 
+# X 1 9999 M 1
+# X 2781480 155701381 M 1
+# Y 1 57227415 M 1
+# Y 1 57227415 F 0
+# MT 1 16569 M 1
+# MT 1 16569 F 1
+# chrX 1 9999 M 1
+# chrX 2781480 155701381 M 1
+# chrY 1 57227415 M 1
+# chrY 1 57227415 F 0
+# chrM 1 16569 M 1
+# chrM 1 16569 F 1
+# *  * *     M 2
+# *  * *     F 2
+# 
+# X
+#    .. Treat male samples as haploid and female as diploid regardless of the chromosome name
+# 
+# *  * *     M 1
+# *  * *     F 2
+# 
+# Y
+#    .. Treat male samples as haploid and female as no-copy, regardless of the chromosome name
+# 
+# *  * *     M 1
+# *  * *     F 0
+# 
+# 1
+#    .. Treat all samples as haploid
+# 
+# *  * *     * 1
+# 
+# Run as --ploidy <alias> (e.g. --ploidy GRCh37).
+# To see the detailed ploidy definition, append a question mark (e.g. --ploidy GRCh37?).
